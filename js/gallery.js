1e5afb42d2c0327c7348c0230104e69449c73653
antonio112518 <antonio3011cas@gmail.com>
2024-11-21 22:38:04 -0600

    imagges

---
+++ js/gallery.js
@@ -0,0 +1,35 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+   // console.log( "ready!" );
+
+
+   $("galleryIMED1316").nanogallery2({
+
+    thumbnailHeight:  "auto",
+    thumbnailWidth:   "450", 
+    thumbnailBorderVertical: "0",
+    thumbnailBorderHorizontel: "0",
+    thumbnailLabel: {
+        position: "overImageOnBottom",
+        display: false
+
+    },
+    thumbnailHoverEffect2: "imageScaleIn80|imageSepiaOff|labelApper75",
+    thumbnailGutterWidth: 20,
+    thumbnailGutterHeight: 20,
+    thumbnailAlignment: "center",
+    thumbnailOpenImage: true
+    
+
+
+
+
+
+
+  });
+  
+
+
+
+
+});
